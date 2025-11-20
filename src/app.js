805cf68320f4890bff896a364db1271e255d3cb2
vinayakbhat123 <abcdbhat014@gmail.com>
2025-11-20 10:42:21 +0530

    Installed cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {connectDB }= require("./config/database")
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
