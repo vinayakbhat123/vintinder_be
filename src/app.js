f680360fc2bd46f2f88571d4dcf79c9db7ea8563
vinayakbhat123 <abcdbhat014@gmail.com>
2025-11-24 17:23:08 +0530

    url added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://vintinder-web.vercel.app/",
   credentials:true,
 }))
 app.use(express.json());
